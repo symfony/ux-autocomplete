f437a81d6eae8ff80ead46f64d68a534700f5c02
Pierre du Plessis <pierre@pcservice.co.za>
2024-02-15 21:26:42 +0200

    [Autocomplete] Reset TomSelect when updating controller attributes

--- assets/src/controller.ts
+++ assets/src/controller.ts
@@ -128,6 +128,10 @@ export default class extends Controller {
         }
     }
 
+    urlValueChanged() {
+        this.resetTomSelect();
+    }
+
     #getCommonConfig(): Partial<TomSettings> {
         const plugins: TPluginHash = {};
 
@@ -391,8 +395,6 @@ export default class extends Controller {
             this.element.innerHTML = currentHtml;
             this.initializeTomSelect();
             this.tomSelect.setValue(currentValue);
-
-            this.startMutationObserver();
         }
     }
 
